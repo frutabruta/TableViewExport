d89ff290c61adfbc6b50022edacda16c4019e557
Pražská integrovaná doprava <52152547+prazska-integrovana-doprava@users.noreply.github.com>
2023-08-16 10:38:57 +0200

    first commit

---
+++ tableviewexport.cpp
@@ -0,0 +1,177 @@
+#include "tableviewexport.h"
+
+TableViewExport::TableViewExport()
+{
+
+}
+
+
+
+void TableViewExport::exportToCsv(QString cesta, QAbstractItemModel *model)
+{
+    qDebug()<<Q_FUNC_INFO;
+    if(model==NULL)
+    {
+        qDebug()<<"pointer je prazdny";
+    }
+
+
+    QFile data(cesta);
+    if (data.open(QFile::WriteOnly | QFile::Truncate))
+    {
+        qDebug()<<"soubor se povedlo otevrit";
+        QTextStream out(&data);
+        out.setEncoding(QStringConverter::System);
+        //out << "Result: " << qSetFieldWidth(10) << left << 3.14 << 2.7;
+        // writes "Result: 3.14      2.7       "
+
+
+        // auto model=ui->tableView_vysledkyDetail->model();
+
+        QStringList hlavicka;
+        for(int i=0;i<model->columnCount();i++)
+        {
+            hlavicka<<QString("\""+model->headerData(i,Qt::Horizontal,Qt::DisplayRole).toString()+"\"");
+
+        }
+        out<<hlavicka.join(";")<<"\r\n";
+
+        for (int i = 0; i < model->rowCount(); i++)
+        {
+            QStringList radek;
+            for (int j = 0; j < model->columnCount(); j++)
+            {
+                //qDebug()<<i<<" "<<j<<" "
+                radek<<QString("\""+model->index(i,j).data(Qt::DisplayRole).toString()+"\"");
+
+            }
+            out<<radek.join(";")<<"\r\n";
+            qDebug()<<radek;
+            //   textData += QLatin1Char('\n');
+        }
+
+
+    }
+    else
+    {
+        qDebug()<<"soubor se nepovedlo otevrit";
+    }
+
+
+
+
+}
+
+void TableViewExport::exportToXml(QString cesta, QAbstractItemModel *model)
+{
+    qDebug()<<Q_FUNC_INFO;
+
+
+}
+
+void TableViewExport::selectExportPath(ExportFileType exportType, QAbstractItemModel *model, QWidget* parent)
+{
+    qDebug()<<Q_FUNC_INFO;
+
+
+    QString fileExtension="";
+    QString fileTypeDescription="";
+
+
+    switch(exportType)
+    {
+    case ExportFileType::Csv:
+        fileExtension="csv";
+        fileTypeDescription="CSV soubor ";
+    break;
+    case ExportFileType::Xml:
+        fileExtension="xml";
+        fileTypeDescription="XML soubor ";
+        break;
+    default:
+        break;
+
+    }
+
+    //ui->tableView_vysledkyDetail->model()
+    if(model==NULL)
+    {
+        qDebug()<<"pointer je prazdny";
+        messageBox("nejdřív vyber test!");
+        return;
+    }
+    qDebug()<<model->rowCount();
+
+    QString cestaSouboru="";
+
+
+
+    QString cestaCsv=otevriSouborDialog(zmenPriponu( cestaSouboru,"csv"),parent,fileExtension,fileTypeDescription);
+    qDebug()<<"cesta csv "<<cestaCsv;
+    if(cestaCsv!="")
+    {
+        if(model->rowCount()>0)
+        {
+            switch(exportType)
+            {
+            case ExportFileType::Csv:
+                exportToCsv(cestaCsv,model);
+                break;
+            case ExportFileType::Xml:
+                exportToXml(cestaCsv,model);
+                break;
+            default:
+                break;
+
+            }
+
+
+        }
+    }
+    else
+    {
+        return;
+    }
+}
+
+
+QString TableViewExport::zmenPriponu(QString vstup,QString pripona)
+{
+    qDebug()<<Q_FUNC_INFO;
+
+    if (vstup.isEmpty())
+    {
+        return "";
+    }
+
+    QString zbytek=vstup.split(".").first();
+    QString staraPripona=vstup.split(".").last();
+
+    qDebug()<<"zbytek: "<<zbytek<<" pripona: "<<staraPripona;
+
+    QString vystup=zbytek+"."+pripona;
+
+    return vystup;
+}
+
+
+
+
+
+QString TableViewExport::otevriSouborDialog(QString cesta, QWidget* parent, QString fileExtension, QString fileTypeDescription)
+{
+    qDebug() <<  Q_FUNC_INFO;
+
+    QString fileName = QFileDialog::getSaveFileName(parent,
+                                                    tr("Otevři soubor"), cesta,
+                                                    fileTypeDescription+" (*."+fileExtension+");;All Files (*)");
+    return fileName;
+}
+
+
+void TableViewExport::messageBox(QString obsah)
+{
+    QMessageBox msgBox;
+    msgBox.setText(obsah);
+    msgBox.exec();
+}
---
+++ tableviewexport.h
@@ -0,0 +1,35 @@
+#ifndef TABLEVIEWEXPORT_H
+#define TABLEVIEWEXPORT_H
+
+#include <QObject>
+#include <QAbstractItemModel>
+#include <QDebug>
+#include <QFile>
+#include <QMessageBox>
+#include <QFileDialog>
+
+enum class ExportFileType
+  {
+     Csv,
+     Xml
+  };
+
+class TableViewExport: QObject
+{
+    Q_OBJECT
+public:
+    TableViewExport();
+    void exportToCsv(QString cesta, QAbstractItemModel *model);
+    void exportToXml(QString cesta, QAbstractItemModel *model);
+
+
+    void selectExportPath(ExportFileType exportType, QAbstractItemModel *model, QWidget *parent);
+
+    QString zmenPriponu(QString vstup, QString pripona);
+    QString otevriSouborDialog(QString cesta, QWidget *parent, QString fileExtension, QString fileTypeDescription);
+private:
+    void messageBox(QString obsah);
+
+};
+
+#endif // TABLEVIEWEXPORT_H
